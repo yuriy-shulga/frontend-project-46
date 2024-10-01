70f5475caf44e2c759a88b45c4d88d8a10619efa
Yuriy Shulga <yshulgag@gmail.com>
2024-10-01 06:54:32 +0300

    Init a new project

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
+
+program.parse();
