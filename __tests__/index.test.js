f82ff26ee6a1555c4ce96c784eeddce99adad5ac
Yuriy Shulga <yshulgag@gmail.com>
2024-10-08 22:23:46 +0300

    Feat (yaml, yml): added support for yaml, yml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,10 +8,16 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('plain json', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
+test('plain', () => {
+  const json1 = getFixturePath('file1.json');
+  const json2 = getFixturePath('file2.json');
+  const yaml1 = getFixturePath('file1.yml');
+  const yaml2 = getFixturePath('file2.yml');
+  const yml1 = getFixturePath('file1.yaml');
+  const yml2 = getFixturePath('file2.yaml');
 
   const expected = fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8');
-  expect(gendiff(filepath1, filepath2)).toEqual(expected);
+  expect(gendiff(json1, json2)).toEqual(expected);
+  expect(gendiff(yaml1, yaml2)).toEqual(expected);
+  expect(gendiff(yml1, yml2)).toEqual(expected);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,13 @@
+import yaml from 'js-yaml';
+
 export default (data, extension) => {
   switch (extension.toLowerCase()) {
     case '.json':
       return JSON.parse(data);
+    case '.yaml':
+      return yaml.load(data);
+    case '.yml':
+      return yaml.load(data);
     default:
       throw new Error(`${extension.slice(1)} format files are not supported!`);
   }
