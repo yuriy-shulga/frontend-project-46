d8bbfd458abc9a56dbd2ba6e75d14803325b1fd8
Yuriy Shulga <yshulgag@gmail.com>
2025-01-08 16:45:55 +0300

    Fix (eslint): move import test, expect, describe

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import gendiff from '../index.js';
 import { describe, test, expect } from '@jest/globals';
+import gendiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
