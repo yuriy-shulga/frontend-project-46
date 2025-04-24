1d65b5c0cdfb22f5aeb63da17ae1f56461a6cea8
Yuriy Shulga <yshulgag@gmail.com>
2025-04-24 19:00:39 +0300

    Fix (eslint): update version to 9.xx

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// eslint.config.mjs
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+])
